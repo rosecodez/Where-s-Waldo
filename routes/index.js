ccb6e21759a7d1bf5ac49d270339294b9e6540aa
rosecodez <rosebeats09@gmail.com>
2024-09-06 17:42:07 -0400

    Create backend route and send images as json response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let prisma = require("../prisma/prisma");
+
+router.get("/images", async function (req, res, next) {
+  try {
+    const images = await prisma.picture.findMany();
+    console.log(images);
+    res.json(images);
+  } catch (err) {
+    next(err);
+  }
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 
 module.exports = router;
